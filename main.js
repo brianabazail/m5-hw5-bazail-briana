1e9cd68ff7a1cc18a32858c8e448e91f52a6d47f
Briana Bazail <brianabazail@ufl.edu>
2019-06-16 09:12:21 -0400

    Add event listeners for messages

--- main.js
+++ main.js
@@ -10,7 +10,16 @@ for (i = 0; i < listItems.length; i++) {
 }
 
 //have a message appear somewhere on the page that says “Oooh - so close, but no cigar”
+listItems[0].addEventListener('click', function(){
+ "Oooh - so close, but no cigar"
+});
 
 //have a message appear somewhere on the page that says “DING DING DING - We have a winner”
+listItems[1].addEventListener('click', function(){
+ "DING DING DING - We have a winner"
+});
 
 //have a message appear somewhere on the page that says “Oops, butter luck next time”
+listItems[2].addEventListener('click', function(){
+ "Oops, butter luck next time"
+});
