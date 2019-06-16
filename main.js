7e10aa8769d231f818d347cbcd7d3948e750ca00
Briana Bazail <brianabazail@ufl.edu>
2019-06-16 02:51:08 -0400

    Finalize highlight section and add js comments

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 //highlight all of the boxes with a different background color
-
 var listItems = document.getElementsByClassName('box');
+
+function changeColor() {
+  this.classList.add('color-change');
+}
+
+for (i = 0; i < listItems.length; i++) {
+  listItems[i].addEventListener('click', changeColor);
+}
+
+//have a message appear somewhere on the page that says “Oooh - so close, but no cigar”
+
+//have a message appear somewhere on the page that says “DING DING DING - We have a winner”
+
+//have a message appear somewhere on the page that says “Oops, butter luck next time”
